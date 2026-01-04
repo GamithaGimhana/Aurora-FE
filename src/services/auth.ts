198e2f25bc3841d48f7f765831ed2090cb4650da
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-04 19:00:16 +0530

    Refactor register function to use Role type for user roles

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,12 @@
 import api from "./api";
+import { type Role } from "../store/auth/authTypes"; 
 
 // REGISTER
 export const register = async (data: {
   name: string;
   email: string;
   password: string;
-  role: string[];  
+  role: Role[];  
 }) => {
   const res = await api.post("/auth/register", data);
   return res.data;
